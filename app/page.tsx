0112bfe98342925900c7dd9956b2389fb6d4b13b
saJeremyQin <343220868@qq.com>
2023-05-29 09:11:37 +0930

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -39,7 +39,8 @@ export default function Home() {
         />
       </div>
 
-      <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left">
+      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left"> */}
+      <div className='mb-32 grid gap-4 lg:mb-0 lg:grid-cols-2 lg:gap-8'>
         <a
           href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
@@ -48,11 +49,12 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Docs{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>
+            </span> */}
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}> */}
+          <p className={`m-0 max-w-[40ch] text-sm opacity-50`} >
             Find in-depth information about Next.js features and API.
           </p>
         </a>
@@ -65,11 +67,11 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Learn{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>
+            </span> */}
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[40ch] text-sm opacity-50`}>
             Learn about Next.js in an interactive course with&nbsp;quizzes!
           </p>
         </a>
@@ -82,11 +84,11 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Templates{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>
+            </span> */}
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[40ch] text-sm opacity-50`}>
             Explore the Next.js 13 playground.
           </p>
         </a>
@@ -99,11 +101,11 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{' '}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
+            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
               -&gt;
-            </span>
+            </span> */}
           </h2>
-          <p className={`m-0 max-w-[30ch] text-sm opacity-50`}>
+          <p className={`m-0 max-w-[40ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
         </a>
---
+++ pages/about.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const AboutPage: React.FC = () => {
+  return <h1>About Page</h1>;
+};
+
+export default AboutPage;
