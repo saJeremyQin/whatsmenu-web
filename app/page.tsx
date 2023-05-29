f49ebc234ac3e3128b5e4204756094a54abb2b81
saJeremyQin <343220868@qq.com>
2023-05-29 11:12:56 +0930

    adjust the divs

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,7 @@
-import Image from 'next/image'
+import Image from 'next/image';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faQrcode } from '@fortawesome/free-solid-svg-icons';
+
 
 export default function Home() {
   return (
@@ -39,61 +42,80 @@ export default function Home() {
         />
       </div>
 
-      {/* <div className="mb-32 grid text-center lg:mb-0 lg:grid-cols-4 lg:text-left"> */}
       <div className='mb-32 grid gap-4 lg:mb-0 lg:grid-cols-2 lg:gap-8'>
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+        <div
+            className="inline-flex group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"    
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Docs{' '}
-            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
-          </h2>
-          {/* <p className={`m-0 max-w-[30ch] text-sm opacity-50`}> */}
-          <p className={`m-0 max-w-[40ch] text-sm opacity-50`} >
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800 hover:dark:bg-opacity-30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <div className="w-1/5 flex items-center justify-center group">
+            <FontAwesomeIcon
+              icon={faQrcode}
+              className="w-1/3 transition-transform group-hover:scale-110"
+            />
+          </div>
+          <div className='flex flex-col ml-6'>
+            <h4 className={`mb-3 text-xl font-semibold`}>
+              Docs{' '}
+            </h4>
+            <p className={`m-0 max-w-[45ch] text-sm opacity-50`} >
+              Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.
+            </p>
+          </div>
+        </div>
+        <div
+            className="inline-flex group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"    
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Learn{' '}
-            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
-          </h2>
-          <p className={`m-0 max-w-[40ch] text-sm opacity-50`}>
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
+          <div className="w-1/5 flex items-center justify-center group">
+            <FontAwesomeIcon
+              icon={faQrcode}
+              className="w-1/3 transition-transform group-hover:scale-110"
+            />
+          </div>
+          <div className='flex flex-col ml-6'>
+            <h4 className={`mb-3 text-xl font-semibold`}>
+              Docs{' '}
+            </h4>
+            <p className={`m-0 max-w-[45ch] text-sm opacity-50`} >
+              Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.
+            </p>
+          </div>
+        </div>
+        <div
+            className="inline-flex group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"    
         >
-          <h2 className={`mb-3 text-2xl font-semibold`}>
-            Templates{' '}
-            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
-          </h2>
-          <p className={`m-0 max-w-[40ch] text-sm opacity-50`}>
-            Explore the Next.js 13 playground.
-          </p>
-        </a>
-
-        <a
+          <div className="w-1/5 flex items-center justify-center group">
+            <FontAwesomeIcon
+              icon={faQrcode}
+              className="w-1/3 transition-transform group-hover:scale-110"
+            />
+          </div>
+          <div className='flex flex-col ml-6'>
+            <h4 className={`mb-3 text-xl font-semibold`}>
+              Docs{' '}
+            </h4>
+            <p className={`m-0 max-w-[45ch] text-sm opacity-50`} >
+              Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.
+            </p>
+          </div>
+        </div>
+        <div
+            className="inline-flex group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"    
+        >
+          <div className="w-1/5 flex items-center justify-center group">
+            <FontAwesomeIcon
+              icon={faQrcode}
+              className="w-1/3 transition-transform group-hover:scale-110"
+            />
+          </div>
+          <div className='flex flex-col ml-6'>
+            <h4 className={`mb-3 text-xl font-semibold`}>
+              Docs{' '}
+            </h4>
+            <p className={`m-0 max-w-[45ch] text-sm opacity-50`} >
+              Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.Find in-depth information about Next.js features and API.
+            </p>
+          </div>
+        </div>
+        {/* <a
           href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
           className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
           target="_blank"
@@ -101,14 +123,11 @@ export default function Home() {
         >
           <h2 className={`mb-3 text-2xl font-semibold`}>
             Deploy{' '}
-            {/* <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span> */}
           </h2>
           <p className={`m-0 max-w-[40ch] text-sm opacity-50`}>
             Instantly deploy your Next.js site to a shareable URL with Vercel.
           </p>
-        </a>
+        </a> */}
       </div>
     </main>
   )
