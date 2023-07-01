048b6f3e5e1ef240441a9a09d3dd3aa180d11ad6
saJeremyQin <343220868@qq.com>
2023-07-01 18:54:27 +0930

    privacy policy page

---
+++ pages/privacy.tsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const PrivacyPage: React.FC = () => {
+  return (
+    <div className="container mx-auto py-8 px-4">
+      <h1 className="text-2xl font-bold mb-4">Privacy Policy</h1>
+      <p className="mb-4">
+        Last updated: [Date]
+        <br />
+        This Privacy Policy describes how [Your Company/App Name] ("we," "us," or "our") collects, uses, and shares personal information when you use our mobile application [App Name] (the "App").
+      </p>
+      <h2 className="text-xl font-bold mb-2">Information We Collect</h2>
+      <h3 className="text-lg font-bold mb-1">1.1 Personal Information:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>Name</li>
+        <li>Email address</li>
+        <li>Phone number</li>
+        <li>Address</li>
+      </ul>
+      <h3 className="text-lg font-bold mb-1">1.2 Usage Data:</h3>
+      <ul className="list-disc list-inside mb-4">
+        <li>App usage statistics</li>
+        <li>Crash reports</li>
+        <li>Device information (e.g., device type, operating system version)</li>
+      </ul>
+      {/* Continue styling the rest of the privacy policy text using similar techniques */}
+    </div>
+  );
+};
+
+export default PrivacyPage;
